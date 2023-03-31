91330200f16fd5e8eb0e8d8c205843116b5d34d0
PARK GI-PYO <gipyopark@gmail.com>
2023-03-31 23:52:56 +0900

    [feat] #5 : spring security - security filter chain 설정
    프로젝트 초기 생성 단계에서 off 해두었던 spring security 설정을 다시 활성화했다.
    현재 개발한 sign up, sign in api 에 대한 접근만 `permitAll()` 처리했다.

--- src/main/java/com/example/projectsns/ProjectSnsApplication.java
+++ src/main/java/com/example/projectsns/ProjectSnsApplication.java
@@ -3,8 +3,10 @@ package com.example.projectsns;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.boot.autoconfigure.jdbc.DataSourceAutoConfiguration;
+import org.springframework.data.jpa.repository.config.EnableJpaAuditing;
 
 @EnableJpaAuditing
+@SpringBootApplication
 public class ProjectSnsApplication {
 
     public static void main(String[] args) {
